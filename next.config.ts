2440a923e85d911b66d080644f7451b98612b35c
ylledolly <dollesinkarylle@gmail.com>
2026-05-11 20:45:03 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbopack: {
-      root: ".",
-    },
-  },
+  /* config options here */
 };
 
 export default nextConfig;
